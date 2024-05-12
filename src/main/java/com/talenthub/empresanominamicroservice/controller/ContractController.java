c67fa1c7823cea20d05505288f8100a45b428f0c
Felipe <juanfeariascastillo@live.com>
2024-05-11 23:47:03 -0500

    1.2.1

--- src/main/java/com/talenthub/empresanominamicroservice/controller/ContractController.java
+++ src/main/java/com/talenthub/empresanominamicroservice/controller/ContractController.java
@@ -1,14 +1,18 @@
 package com.talenthub.empresanominamicroservice.controller;
+/**
+ * Developed by: Juan Felipe Arias.
+ */
 
 import com.talenthub.empresanominamicroservice.model.Contract;
-import com.talenthub.empresanominamicroservice.model.Employee;
 import com.talenthub.empresanominamicroservice.service.ContractService;
-import com.talenthub.empresanominamicroservice.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+/**
+ * Controller to manage the petitions related to contracts.
+ */
 @RestController
 @RequestMapping("/contract")
 public class ContractController {
@@ -16,24 +20,55 @@ public class ContractController {
     @Autowired
     private ContractService contractService;
 
+    /**
+     * @name getAllContracts
+     * @description Retrieves all existing contracts.
+     *
+     * @return An iterable list of contracts.
+     */
     @CrossOrigin
     @GetMapping("/getContracts")
     public Iterable<Contract> getAllContracts() {
         return contractService.getAll();
     }
 
+    /**
+     * @name getContractById
+     * @description Retrieves a contract by its Id.
+     *
+     * @param id The Id of the contract to find.
+     *
+     * @return The contract with the specified Id, if existed.
+     */
     @CrossOrigin
     @GetMapping("/{id}")
     public Optional<Contract> getContractById(@PathVariable Long id) {
         return contractService.getById(id);
     }
 
+    /**
+     * @name createContract
+     * @description Creates a new contract.
+     *
+     * @param contract The information of the contract before created.
+     *
+     * @return A newly created contract.
+     */
     @CrossOrigin
     @PostMapping("/createContract")
     public Contract createContract(@RequestBody Contract contract) {
         return contractService.create(contract);
     }
 
+    /**
+     * @name updateContract
+     * @description Creates a new contract.
+     *
+     * @param id The Id of the contract to update.
+     * @param contractDetails The details of the updated contract.
+     *
+     * @return The updated contract.
+     */
     @CrossOrigin
     @PutMapping("/updateContract/{id}")
     public Contract updateContract(@PathVariable Long id, @RequestBody Contract contractDetails){
--- src/main/java/com/talenthub/empresanominamicroservice/service/EmployeeService.java
+++ src/main/java/com/talenthub/empresanominamicroservice/service/EmployeeService.java
@@ -1,4 +1,7 @@
 package com.talenthub.empresanominamicroservice.service;
+/**
+ * Develop by: Juan Felipe Arias
+ */
 
 import com.talenthub.empresanominamicroservice.model.Employee;
 import com.talenthub.empresanominamicroservice.repository.EmployeeRepository;
@@ -7,24 +10,54 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+/**
+ * Service class for managing employees.
+ */
 @Service
 public class EmployeeService {
 
     @Autowired
     private EmployeeRepository EmployeeRepository;
 
+    /**
+     * @name getAll
+     * @description Retrieves all existing employees.
+     *
+     * @return An iterable list of employees.
+     */
     public Iterable<Employee> getAll(){
         return EmployeeRepository.findAll();
     }
 
+    /**
+     * @name getById
+     * @description Retrieves an employee by its ID.
+     *
+     * @param id the ID of the employee.
+     * @return An optional containing the employee with the specified ID, if exists.
+     */
     public Optional<Employee> getById(Long id){
         return EmployeeRepository.findById(id);
     }
 
+    /**
+     * @name create
+     * @description Creates a new employee.
+     *
+     * @param employee the details of the employee to create.
+     * @return The newly created employee.
+     */
     public Employee create(Employee employee){
         return EmployeeRepository.save(employee);
     }
 
+    /**
+     * @name update
+     * @description Updates an existing employee.
+     *
+     * @param employee the employee to update.
+     * @return The updated employee.
+     */
     public Employee update(Employee employee){
         return EmployeeRepository.save(employee);
     }
