1f07cd92ffaf96d6ef6ece7a1e97e6fd66bc127f
Sebastian Orjuela <sebasorjuela@gmail.com>
2024-05-22 22:46:44 -0500

    Add lombok and fix the properties

--- src/main/java/com/talenthub/empresanominamicroservice/model/News.java
+++ src/main/java/com/talenthub/empresanominamicroservice/model/News.java
@@ -1,7 +1,11 @@
 package com.talenthub.empresanominamicroservice.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "news", schema = "payrollDb")
 public class News {
@@ -10,45 +14,12 @@ public class News {
     @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Column(name = "name", length = 45)
+    @Column(name = "name")
     private String name;
 
-    @Column(name = "description", length = 45)
+    @Column(name = "description")
     private String description;
 
     @Column(name = "moneybenefit")
     private Double moneybenefit;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getMoneybenefit() {
-        return moneybenefit;
-    }
-
-    public void setMoneybenefit(Double moneybenefit) {
-        this.moneybenefit = moneybenefit;
-    }
-
 }
